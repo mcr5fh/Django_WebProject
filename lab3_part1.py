4f0e3b4d6cd5108aa7aedefc3f6b7e07896d3d48
cahruhr <cahruhr2795@gmail.com>
2015-10-12 17:20:15 -0400

    add previous lab files

---
+++ lab3_part1.py
@@ -0,0 +1,24 @@
+from Crypto.Hash import SHA256
+
+# random comment
+
+users = {}
+user_count = 0
+while True:
+    user_id = input('user #' + str(user_count) + ': ')
+    if user_id == '':
+        break
+    users[user_id] = SHA256.new(str.encode(input('password: '))).hexdigest()
+    user_count += 1
+
+while True:
+    user_id = input('username: ')
+    if user_id not in users:
+        print('user not found')
+        continue
+    password = input('password: ')
+    if users[user_id] != SHA256.new(str.encode(password)).hexdigest():
+        print('login fails')
+    else:
+        print('login succeeds')
+
---
+++ lab3_part4.py
@@ -0,0 +1,15 @@
+from Crypto.Cipher import DES
+import json
+
+key = input('key: ')
+msg = input('message: ')
+
+des = DES.new(key, DES.MODE_ECB)
+encrypted = des.encrypt(msg)
+
+#print(encrypted)
+
+encrypted_file = open('secret.txt', '+w')
+json.dump(encrypted.decode('latin-1'), encrypted_file)
+
+
