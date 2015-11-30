978f388d276b332da3d8af70418a2b71b0367774
cahruhr <cahruhr2795@gmail.com>
2015-11-30 17:22:11 -0500

    add more functionality to secureshare_FDA.py

--- native/secureshare_FDA.py
+++ native/secureshare_FDA.py
@@ -1,4 +1,4 @@
-import urllib.request
+import urllib.request, urllib.error
 import json
 import getpass
 
@@ -13,17 +13,24 @@ def authenticate():
     USER = input('username: ')
     PASS = getpass.getpass('password: ')
 
-#authenticate()
+authenticate()
 url = DEPLOY_URL + 'api/list'
 
 filename, _ = urllib.request.urlretrieve(url) # download the file and keep its name
 reports = json.load(open(filename))
 
-#print(reports)
+print('List of reports and (attached files): ')
 
 i = 0
 for report in reports:
     print(str(i) + ': ' + report['short'] + ' (' + report['url'].split('/')[-1] + ')')
     i += 1
 
-which = input('select a report to download (0-' + str(i-1) + '): ')
+which = int(input('select a report to download (0-' + str(i-1) + '): '))
+
+download_url = DEPLOY_URL + reports[which]['url']
+try: 
+    download_filename, _ = urllib.request.urlretrieve(download_url) # download the file and keep its name
+    print('downloaded file to ' + download_filename)
+except urllib.error.HTTPError:
+    print('This report does not contain a file.')
