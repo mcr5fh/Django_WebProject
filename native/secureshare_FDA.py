94677d771eb4609fb2d802d00d9a327418d60a03
cahruhr <cahruhr2795@gmail.com>
2015-12-07 19:28:56 -0500

    add file encryption to standalone app

--- native/secureshare_FDA.py
+++ native/secureshare_FDA.py
@@ -3,6 +3,7 @@ import json
 import getpass
 import requests
 import sys
+from simplecrypt import encrypt, decrypt
 
 def download_files():
     #url = input('Enter a URL: ')
@@ -50,19 +51,56 @@ def download_files():
 
     print('done')
 
+def encrypt_file():
+    filename = input('filename: ')
+    password = input('encryption password: ')
+
+    print('writing to ' + filename + '.encrypted')
+
+    in_file = open(filename, 'rb')
+    data = in_file.read()
+    in_file.close()
+
+    ciphertext = encrypt(password, data)
+    
+    out_file = open(filename + '.encrypted', 'wb+')
+    out_file.write(ciphertext)
+    out_file.close()
+
+def decrypt_file():
+    filename = input('filename: ')
+    password = input('encryption password: ')
+
+    if filename.split('.')[-1] == 'encrypted':
+        out_filename = '.'.join(filename.split('.')[0:-1])
+        print('writing to ' + out_filename)
+    else:
+        out_filename = filename
+        print('extension is not .encrypted; overwriting original file')
+
+    in_file = open(filename, 'rb')
+    data = in_file.read()
+    in_file.close()
+
+    plaintext = decrypt(password, data)
+
+    out_file = open(out_filename, 'wb+')
+    out_file.write(plaintext)
+    out_file.close()
+
 def main():
     print('Available functions: ')
     print('0. Download files')
-    print('1. Encrypt local files')
-    print('2. Decrypt local files')
+    print('1. Encrypt local file')
+    print('2. Decrypt local file')
     function = int(input('Select a function (0-2): '))
 
     if function == 0:
         download_files()
     elif function == 1:
-        pass
+        encrypt_file()
     elif function == 2:
-        pass
+        decrypt_file()
         
 if __name__ == '__main__':
     main()
